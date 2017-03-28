f78004aae37bc0a306193b83944575113b6efae5
rasberryrabbit <lovelust@hanmail.net>
2017-03-28 19:27:38 +0900

    Add nginx.conf modification for multi streaming. Add process priority function for nginx but not fully work. currently disabled.

--- nginxtool_main.pas
+++ nginxtool_main.pas
@@ -15,10 +15,14 @@ type
   TFormNginxtool = class(TForm)
     Button1: TButton;
     Button2: TButton;
+    CheckBox1: TCheckBox;
+    CheckBoxModConf: TCheckBox;
     Panel1: TPanel;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure CheckBoxModConfClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure Panel1Click(Sender: TObject);
   private
@@ -37,6 +41,10 @@ uses
 
 var
   loglist:TLogListFPC;
+  {$ifdef WINDOWS}
+  UseAboveNormalProcess:Boolean;
+  {$endif}
+  checkflag:string = '';
 
 
 {$R *.lfm}
@@ -126,6 +134,48 @@ begin
     end;
   end;
 end;
+
+(*
+procedure ProcessPriority(const name:string; priority:dword);
+var
+   processids:array[0..1023] of DWord;
+   cbNeed, pCount, i:DWord;
+   ProcessName:array[0..MAX_PATH-1] of char;
+   function ProcessPriorityHandle(pid:DWORD):Boolean;
+   var
+     ph:HANDLE;
+     pm:HMODULE;
+     cbval:DWORD;
+   begin
+     result:=false;
+     ph:=OpenProcess(PROCESS_QUERY_INFORMATION or PROCESS_VM_READ or PROCESS_SET_INFORMATION, false, processids[i]);
+     if ph<>0 then begin
+       if EnumProcessModules(ph,@pm,sizeof(pm),cbval) then begin
+         GetModuleBaseName(ph,pm,ProcessName,sizeof(ProcessName) div SizeOf(char));
+         if CompareText(name,strpas(ProcessName))=0 then begin
+           if not (GetPriorityClass(ph)=priority) then
+             if not SetPriorityClass(ph,priority) then begin
+               GetLastError;
+               loglist.AddLog(Format('Cannot set process priority %d',[ph]));
+             end else begin
+               loglist.AddLog(Format('Set process priority %d',[ph]));
+             end;
+         end;
+       end;
+     end;
+     CloseHandle(ph);
+   end;
+begin
+  if EnumProcesses(@processids[0],sizeof(processids),cbNeed) then begin
+    pCount:=cbNeed div sizeof(DWord);
+    for i:=0 to pCount-1 do begin
+      if processids[i]<>0 then begin
+        ProcessPriorityHandle(processids[i]);
+      end;
+    end;
+  end;
+end;
+*)
 {$endif}
 
 { TFormNginxtool }
@@ -137,26 +187,103 @@ end;
 
 procedure TFormNginxtool.VerboseNginxConfig;
 var
- buf : string;
+ buf, bufrtmp : string;
  fs : TFileStream;
  bufsize, bufloc, bufpos, bufopen : Integer;
- rx : TRegExpr;
+ rx, rxrtmp : TRegExpr;
  templist : TStringList;
+ workercount, chunksize : Integer;
+ has_autopush, chunk_modified : Boolean;
 begin
  try
    // read config file
    fs := TFileStream.Create('conf/nginx.conf',fmOpenRead or fmShareDenyNone);
    try
      bufsize := fs.Size;
-     if bufsize>65530 then
-        bufsize:=65530;
+     if bufsize>131072 then
+        bufsize:=131072;
      SetLength(buf,bufsize);
-     fs.Read(buf[1],fs.Size);
+     fs.Read(buf[1],bufsize);
    finally
      fs.Free;
    end;
  except
    buf:='';
+ end;
+   // check 'worker_processes 1;'
+   rx := TRegExpr.Create('worker_processes\s+(\d+)\;');
+   try
+     rx.ModifierI:=True;
+     if rx.Exec(buf) then begin
+       workercount:=StrToIntDef(rx.Match[1],0);
+       loglist.AddLog(rx.Match[0]);
+     end else
+       workercount:=0;
+   finally
+     rx.Free;
+   end;
+   // check 'rtmp_auto_push on;'
+   rx := TRegExpr.Create('rtmp_auto_push\s+([a-zA-Z]+)\;');
+   try
+     rx.ModifierI:=True;
+     has_autopush:=False;
+     if rx.Exec(buf) then begin
+       has_autopush:=UpperCase(rx.Match[1])='ON';
+       if not has_autopush then begin
+         buf:=rx.Replace(buf,'',False);
+       end;
+       loglist.AddLog(rx.Match[0]);
+     end;
+   finally
+     rx.Free;
+   end;
+ if CheckBoxModConf.Checked then begin
+   // check 'chunk_size 8192;'
+   rx := TRegExpr.Create('rtmp\s+\{.+server\s+\{.+chunk_size\s+(\d+)\;');
+   try
+     rx.ModifierI:=True;
+     if rx.Exec(buf) then begin
+       if StrToIntDef(rx.Match[1],0)<8192 then begin
+         buf:=Copy(buf,1,rx.MatchPos[1]-1)+'8192'+Copy(buf,rx.MatchPos[1]+rx.MatchLen[1]);
+         chunk_modified:=True;
+       end;
+     end else begin
+       rxrtmp:=TRegExpr.Create('rtmp\s+\{[^\{]+server\s+\{\s+[^;]+;');
+       try
+         rxrtmp.ModifierI:=True;
+         if rxrtmp.Exec(buf) then begin
+           buf:=Copy(buf,1,rxrtmp.MatchPos[0]+rxrtmp.MatchLen[0]-1)+#10#9#9'chunk_size 8192;'#10+
+                Copy(buf,rxrtmp.MatchPos[0]+rxrtmp.MatchLen[0]);
+           chunk_modified:=True;
+         end;
+       finally
+         rxrtmp.Free;
+       end;
+     end;
+   finally
+     rx.Free;
+   end;
+   // add 'rtmp_auto_push on'
+   if (workercount<>1) and (not has_autopush) then begin
+     buf:='rtmp_auto_push on;'#10+buf;
+     chunk_modified:=True;
+     loglist.AddLog('rtmp_auto_push on; added');
+   end;
+   // update nginx.conf
+   if chunk_modified then begin
+     try
+       fs:=TFileStream.Create('conf/nginx.conf',fmOpenReadWrite or fmShareDenyNone);
+       try
+         fs.Write(buf[1],Length(buf));
+       finally
+         fs.Free;
+       end;
+     except
+       on e:exception do begin
+         loglist.AddLog('Fail to write file "conf/nginx.conf"');
+       end;
+     end;
+   end;
  end;
  // parse config file
  rx := TRegExpr.Create('rtmp\s+\{');
@@ -177,9 +304,9 @@ begin
      end;
      templist:=TStringList.Create;
      try
-       buf:=Copy(buf,bufloc,bufpos-bufloc);
-       buf:=StringReplace(buf,#9,#32#32#32#32,[rfReplaceAll]);
-       templist.Text:=buf;
+       bufrtmp:=Copy(buf,bufloc,bufpos-bufloc);
+       bufrtmp:=StringReplace(bufrtmp,#9,#32#32#32#32,[rfReplaceAll]);
+       templist.Text:=bufrtmp;
        for bufpos:=0 to templist.Count-1 do begin
          loglist.AddLog(templist.Strings[bufpos]);
        end;
@@ -202,10 +329,35 @@ begin
   loglist.LineLimit:=1000;
 end;
 
+procedure TFormNginxtool.FormDestroy(Sender: TObject);
+var
+  fs : TFileStream;
+  s : string;
+  i : Integer;
+begin
+  s:='00';
+  if CheckBox1.Checked then
+   s[1]:='1';
+  if CheckBoxModConf.Checked then
+   s[2]:='1';
+  if checkflag<>s then begin
+    try
+      fs:=TFileStream.Create('nginxtool.ini',fmOpenWrite or fmCreate or fmShareDenyNone);
+      try
+        fs.Write(s[1],Length(s));
+      finally
+        fs.Free;
+      end;
+    except
+    end;
+  end;
+end;
+
 procedure TFormNginxtool.Button1Click(Sender: TObject);
 var
   myprocess:TProcess;
   i:integer;
+  ppval:DWORD;
 begin
   {$ifdef WINDOWS}
   if checkEnumProcess('nginx.exe') then begin
@@ -221,8 +373,25 @@ begin
     for i:=1 to GetEnvironmentVariableCount do
       myprocess.Environment.Add(GetEnvironmentString(i));
     myprocess.Executable:='nginx';
+    {$ifdef WINDOWS}
+    if CheckBox1.Checked then
+      if UseAboveNormalProcess then begin
+        myprocess.Priority:=ppAboveNormal;
+        ppval:=ABOVE_NORMAL_PRIORITY_CLASS;
+      end
+      else begin
+        myprocess.Priority:=ppHigh;
+        ppval:=HIGH_PRIORITY_CLASS;
+      end;
+    {$endif}
     try
       myprocess.Execute;
+      (*
+      {$ifdef WINDOWS}
+      if CheckBox1.Checked then
+        ProcessPriority('nginx.exe',ppval);
+      {$endif}
+      *)
       loglist.AddLog('> nginx started');
     except
       on e:exception do
@@ -266,6 +435,37 @@ begin
   end;
 end;
 
+procedure TFormNginxtool.CheckBoxModConfClick(Sender: TObject);
+begin
+  if CheckBoxModConf.Checked then
+    VerboseNginxConfig;
+end;
+
+function ReadINIFile(idx:Integer):Boolean;
+var
+  fs : TFileStream;
+  s : string;
+begin
+  s:='01';
+  if idx>Length(s) then
+    idx:=Length(s);
+  if checkflag='' then begin
+    try
+      fs:=TFileStream.Create('nginxtool.ini',fmOpenRead or fmShareDenyNone);
+      try
+        fs.Read(s[1],Length(s));
+      finally
+        fs.Free;
+      end;
+    except
+      s:='01';
+    end;
+    checkflag:=s;
+  end;
+  Result:=checkflag[idx]='1';
+end;
+
+
 procedure TFormNginxtool.FormShow(Sender: TObject);
 var
   IPBuf:array[0..254] of char;
@@ -273,7 +473,26 @@ begin
   VerboseNginxConfig;
   GetIPAddr(IPBuf,sizeof(IPBuf));
   loglist.AddLog(Format('> IP Address: %s',[IPBuf]));
+  CheckBox1.Checked:=ReadINIFile(1);
+  CheckBoxModConf.Checked:=ReadINIFile(2);
 end;
+
+{$ifdef WINDOWS}
+function CheckWinVer:Boolean;
+var
+  Os:OSVERSIONINFO;
+begin
+  Result:=False;
+  Os.dwOSVersionInfoSize:=sizeof(OSVERSIONINFO);
+  if GetVersionEx(Os) then
+    Result:=Os.dwMajorVersion>=5;
+end;
+
+initialization
+  UseAboveNormalProcess:=CheckWinVer;
+
+{$endif}
+
 
 end.
 
